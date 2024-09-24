cabd3bdf6089bc858d383cab2f07a3e5a50524af
FireLord <firelord.xda@gmail.com>
2024-09-24 09:56:21 +0530

    privacy: fix privacy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,72 +1,72 @@
-export function Privacy() {
+export default function Privacy() {
     return (
-        <div className="bg-white">
-            <h1>CapCap - Terms and Conditions</h1>
+        <>
+            <div className="bg-white">
+                <h1>CapCap - Terms and Conditions</h1>
 
-            <h2>Introduction</h2>
-            <p>
-                Welcome to our CapCap App. These terms and conditions outline the rules and regulations
-                for the use of our app.
-                By accessing or using our app, you accept and agree to be bound by these terms and conditions.
-            </p>
+                <h2>Introduction</h2>
+                <p>
+                    Welcome to our CapCap App. These terms and conditions outline the rules and regulations
+                    for the use of our app.
+                    By accessing or using our app, you accept and agree to be bound by these terms and conditions.
+                </p>
 
-            <h2>Intellectual Property Rights</h2>
-            <p>
-                Unless otherwise stated, we own the intellectual property rights for the app and its content.
-                All intellectual property rights are reserved.
-            </p>
+                <h2>Intellectual Property Rights</h2>
+                <p>
+                    Unless otherwise stated, we own the intellectual property rights for the app and its content.
+                    All intellectual property rights are reserved.
+                </p>
 
-            <h2>Generated Subtitles</h2>
-            <p>
-                Our app provides automated subtitle generation services based on the videos you upload.
-                It's important to note that the accuracy and quality of the generated subtitles may vary and may not be perfect.
-                We do not guarantee the correctness or completeness of the subtitles.
-            </p>
+                <h2>Generated Subtitles</h2>
+                <p>
+                    Our app provides automated subtitle generation services based on the videos you upload.
+                    Its important to note that the accuracy and quality of the generated subtitles may vary and may not be perfect.
+                    We do not guarantee the correctness or completeness of the subtitles.
+                </p>
 
-            <h2>User Responsibilities</h2>
-            <p>
-                By using our app, you agree not to use it for any illegal or unauthorized purpose.
-                You must not misuse the app, interfere with its functionality, or attempt to access data or features you are not
-                authorized to use.
-            </p>
+                <h2>User Responsibilities</h2>
+                <p>
+                    By using our app, you agree not to use it for any illegal or unauthorized purpose.
+                    You must not misuse the app, interfere with its functionality, or attempt to access data or features you are not
+                    authorized to use.
+                </p>
 
-            <h2><a href="https://www.capcap.in/privacy">Privacy Policy</a></h2>
-            <p>
-                Our app collects and processes personal information as outlined in our Privacy Policy.
-                By using our app, you consent to the collection and use of your data as described in the Privacy Policy.
-            </p>
+                <h2><a href="https://www.capcap.in/privacy">Privacy Policy</a></h2>
+                <p>
+                    Our app collects and processes personal information as outlined in our Privacy Policy.
+                    By using our app, you consent to the collection and use of your data as described in the Privacy Policy.
+                </p>
 
-            <h2>Limitation of Liability</h2>
-            <p>
-                Our app and its owners, employees, or affiliates will not be held liable for any direct, indirect,
-                consequential, or incidental damages arising out of the use or inability to use the app.
-            </p>
+                <h2>Limitation of Liability</h2>
+                <p>
+                    Our app and its owners, employees, or affiliates will not be held liable for any direct, indirect,
+                    consequential, or incidental damages arising out of the use or inability to use the app.
+                </p>
 
-            <h2>Indemnification</h2>
-            <p>
-                You agree to indemnify and hold harmless our app and its owners, employees, or affiliates from and against any
-                claims, damages, liabilities, losses, and expenses, arising out of your use of the app or violation of these
-                terms and conditions.
-            </p>
+                <h2>Indemnification</h2>
+                <p>
+                    You agree to indemnify and hold harmless our app and its owners, employees, or affiliates from and against any
+                    claims, damages, liabilities, losses, and expenses, arising out of your use of the app or violation of these
+                    terms and conditions.
+                </p>
 
-            <h2>Changes to Terms and Conditions</h2>
-            <p>
-                We reserve the right to modify or replace these terms and conditions at any time. The updated version will be
-                posted on this page.
-            </p>
+                <h2>Changes to Terms and Conditions</h2>
+                <p>
+                    We reserve the right to modify or replace these terms and conditions at any time. The updated version will be
+                    posted on this page.
+                </p>
 
-            <h2>Contact Us</h2>
-            <p>
-                If you have any questions or concerns about these terms and conditions, please contact us at:
+                <h2>Contact Us</h2>
+                <p>
+                    If you have any questions or concerns about these terms and conditions, please contact us at:
 
-                Email: support@capcap.in
-            </p>
+                    Email: support@capcap.in
+                </p>
 
-            <footer>
-                <p>Last updated: 30th July 2023</p>
-            </footer>
-        </div>
+                <footer>
+                    <p>Last updated: 30th July 2023</p>
+                </footer>
+            </div>
+        </>
     );
 }
-
-export default Privacy;
